1a1d9b49a0bed34cb1f45b519e86863d1df54c83
Roman <kamazaff22@gmail.com>
2019-10-04 13:15:06 +0300

    more css

--- portfolio-app/src/ui/Portfolio.js
+++ portfolio-app/src/ui/Portfolio.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import style from './Portfolio.module.css';
 import MenuBlock from './menuBlock/MenuBlock';
 import logoProfile from '../img/profileInfo.png';
@@ -6,9 +6,19 @@ import contacts from '../img/contacts.png';
 import myWorks from '../img/myWorks.png';
 import myFoto from '../img/I.jpg';
 
+const block = {
+    openInfoBlock: 'openInfoBlock',
+    openMyWorksBlock: 'openMyWorksBlock',
+    openMyContactsBlock: 'openMyContactsBlock',
+    closedAll: 'closedAll',
+}
+
 
 
 let Portfolio = (props) => {
+
+    const [openBlock, setopenBlock] = useState(block.closedAll);
+
     return <div className={style.app}>
         <div className={style.portfolioApp}>
             <div className={style.myFoto}> <img src={myFoto} /> </div>
@@ -17,10 +27,22 @@ let Portfolio = (props) => {
                 <div className={style.name}>kurilovich</div>
                 <div className={style.specialty}>Junior react developer</div>
             </div>
-            <div className={style.aboutMe}> <MenuBlock logoProfile={logoProfile} color="#4AB6EE" /> </div>
-            <div className={style.myWorks}> <MenuBlock logoProfile={myWorks} color="#A6C41F" /></div>
-            <div className={style.contacts}> <MenuBlock logoProfile={contacts} color="#7B9F00" /></div>
+
+            <div className={style.aboutMe} onClick={()=>setopenBlock(block.openInfoBlock)}> <MenuBlock logoProfile={logoProfile} color="#4AB6EE" /> </div>
+            <div className={style.myWorks} onClick={()=>setopenBlock(block.openMyWorksBlock)}> <MenuBlock logoProfile={myWorks} color="#A6C41F" /></div>
+            <div className={style.contacts} onClick={()=>setopenBlock(block.openMyContactsBlock)}> <MenuBlock logoProfile={contacts} color="#7B9F00" /></div>
+
+            { openBlock === block.openInfoBlock && <div className={style.myInfo}>
+                tut rodilsay tut uchilsay
+            </div>}
+            { openBlock === block.openMyWorksBlock && <div className={style.aboutMyWorks}>
+                tut moi raboti
+            </div>}
+            { openBlock === block.openMyContactsBlock && <div className={style.aboutMyContacts}>
+                tut moi kinacti
+            </div>}
         </div>
+
     </div>
 }
 
