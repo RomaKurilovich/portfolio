29bba9aa30068ff7995bfa8668290c31643ff7c6
Roman <kamazaff22@gmail.com>
2019-10-07 15:59:43 +0300

    add gh-pages

---
+++ precache-manifest.37435798b1c558f5d4d28e9a296abdc9.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc21d47d54faffb83ff691ab6a2c8521",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "b0f22688f7879a115551",
+    "url": "/portfolio/static/css/main.cfea1f33.chunk.css"
+  },
+  {
+    "revision": "5f91b3cd7255342cfa06",
+    "url": "/portfolio/static/js/2.2130d192.chunk.js"
+  },
+  {
+    "revision": "b0f22688f7879a115551",
+    "url": "/portfolio/static/js/main.29120390.chunk.js"
+  },
+  {
+    "revision": "dc461e139f7b8386b731",
+    "url": "/portfolio/static/js/runtime-main.1a734054.js"
+  },
+  {
+    "revision": "d40df8964ff6937efed4d3548aafdbb0",
+    "url": "/portfolio/static/media/I.d40df896.jpg"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/portfolio/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "a1f947d712e7db8d938d502eca6db7e9",
+    "url": "/portfolio/static/media/portfolio.a1f947d7.png"
+  },
+  {
+    "revision": "d571004daa97cf0df840edfc82e01c21",
+    "url": "/portfolio/static/media/socialnetwork.d571004d.png"
+  },
+  {
+    "revision": "72d7df5f89e078b596940d236df152de",
+    "url": "/portfolio/static/media/todo.72d7df5f.png"
+  },
+  {
+    "revision": "48f04fac5c7d1f94a0fb680f336cc35e",
+    "url": "/portfolio/static/media/weather.48f04fac.png"
+  }
+]);
